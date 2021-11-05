ef9de09d93134e644de4c302d25fff71cc714c96
Todor Konjevic <todor.konjevic@gmail.com>
2021-11-05 10:25:44 +0100

    Add tabs to Home Screen.

--- src/features/home/HomeScreen.tsx
+++ src/features/home/HomeScreen.tsx
@@ -1,6 +1,8 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React, { useLayoutEffect } from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { setListener } from 'appcenter-crashes';
+import React, { useLayoutEffect, useState } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
+import MaterialTabs from 'react-native-material-tabs';
 import { IconButton } from 'react-native-paper';
 import BookItem from '../../core/components/BookItem';
 import useBooks from '../../core/hooks/useBooks';
@@ -10,6 +12,7 @@ import { Book } from '../../core/schemas/BookSchema';
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 const HomeScreen = ({ navigation }: Props) => {
+  const [selectedTab, setSelectedTab] = useState<number>(0);
   const { books } = useBooks();
 
   useLayoutEffect(() => {
@@ -24,12 +27,19 @@ const HomeScreen = ({ navigation }: Props) => {
   }, [navigation]);
 
   return (
-    <FlatList
-      data={books}
-      keyExtractor={(book) => book._id}
-      renderItem={renderItem}
-      style={style.container}
-    />
+    <View>
+      <MaterialTabs
+        items={['All', 'Unread']}
+        selectedIndex={selectedTab}
+        onChange={setSelectedTab}
+      />
+      <FlatList
+        data={selectedTab === 0 ? books : books.filter((book) => !book.read)}
+        keyExtractor={(book) => book._id}
+        renderItem={renderItem}
+        style={style.container}
+      />
+    </View>
   );
 };
 
